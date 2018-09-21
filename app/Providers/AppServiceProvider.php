188f35184ee32839491b812233a4fd41ef93efa1
timoffmax <tim.off.max@gmail.com>
2018-09-21 14:23:48 +0300

    Add a custom directive to include SVG images to Blade templates

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,7 +14,24 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        // DB fix
         Schema::defaultStringLength(191);
+
+        // Use SVG
+        \Blade::directive('svg', function($arguments) {
+            // Funky madness to accept multiple arguments into the directive
+            list($path, $class) = array_pad(explode(',', trim($arguments, "() ")), 2, '');
+            $path = trim($path, "' ");
+            $class = trim($class, "' ");
+
+            // Create the dom document as per the other answers
+            $svg = new \DOMDocument();
+            $svg->load(public_path($path));
+            $svg->documentElement->setAttribute("class", $class);
+            $output = $svg->saveXML($svg->documentElement);
+
+            return $output;
+        });
     }
 
     /**
