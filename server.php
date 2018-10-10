f54702d8c46d7f63f0e968152cbb6b7082371734
timoffmax <tim.off.max@gmail.com>
2018-10-10 07:49:48 +0300

    Fix a strange bug with illegal characters in developer server's response (https://stackoverflow.com/questions/23274475/artisan-serve-send-assets-with-response-headers-embedded/39081171)

--- server.php
+++ server.php
@@ -15,6 +15,7 @@ $uri = urldecode(
 // built-in PHP web server. This provides a convenient way to test a Laravel
 // application without having installed a "real" web server software here.
 if ($uri !== '/' && file_exists(__DIR__.'/public'.$uri)) {
+    ob_end_clean ();
     return false;
 }
 
