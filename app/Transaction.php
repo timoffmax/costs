58bd3e5f3338c27f59d36173f25062f79d1e40fc
timoffmax <tim.off.max@gmail.com>
2018-11-19 17:16:56 +0200

    Added some relations to Transaction model Added some methods to fetch info

--- app/Transaction.php
+++ app/Transaction.php
@@ -2,20 +2,24 @@
 
 namespace App;
 
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 /**
  * Class Transaction
  *
  * @property int $type_id
+ * @property int $account_id
  * @property int $user_id
  * @property float $sum
+ * @property float $balance_before
+ * @property float $balance_after
  * @property string $date
  * @property string $comment
  * @property TransactionType $type
- * @property User $user
+ * @property Account $account
  */
-class Transaction extends Model
+class Transaction extends BaseModel
 {
     /**
      * The table associated with the model.
@@ -30,7 +34,7 @@ class Transaction extends Model
      * @var array
      */
     protected $fillable = [
-        'type_id', 'user_id', 'sum', 'date', 'comment',
+        'type_id', 'account_id', 'sum', 'date', 'comment',
     ];
 
     /**
@@ -40,6 +44,56 @@ class Transaction extends Model
      */
     protected $hidden = [];
 
+    /**
+     * Returns array of transactions, filtered and paginated
+     *
+     * @param Request $request
+     * @param User|null $user
+     * @return Transaction|\Illuminate\Contracts\Pagination\LengthAwarePaginator|\Illuminate\Database\Eloquent\Builder
+     */
+    public static function getTransactions(Request $request, ?User $user = null)
+    {
+        $pageSize = $request['pageSize'] ?? self::DEFAULT_PAGE_SIZE;
+
+        if (!empty($user)) {
+            $result = self::getUserTransactions($user);
+        } else {
+            $result = self::getAllTransactions();
+        }
+
+        $result = $result->latest();
+        $result = $result->paginate($pageSize);
+
+        return $result;
+    }
+
+    /**
+     * Get transactions of all users
+     *
+     * @return Transaction|\Illuminate\Database\Eloquent\Builder
+     */
+    protected static function getAllTransactions()
+    {
+        return self::with('user')
+            ->with('type')
+            ->with('account')
+        ;
+    }
+
+    /**
+     * Get transactions of particular user
+     *
+     * @param User $user
+     * @return Transaction|\Illuminate\Database\Eloquent\Builder
+     */
+    protected static function getUserTransactions(User $user)
+    {
+        return self::with('user')
+            ->with('account')
+            ->with('type')
+            ->where(['user_id' => $user])
+        ;
+    }
 
     /**
      * Get the transaction owner
@@ -49,6 +103,16 @@ class Transaction extends Model
     public function user()
     {
         return $this->belongsTo(User::class);
+    }
+
+    /**
+     * Get the transaction account
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function account()
+    {
+        return $this->belongsTo(Account::class);
     }
 
     /**
